97104a9543af318c58897efafcac80f65969058b
Philip Heindel <p.e.heindel@gmail.com>
2020-06-18 12:43:15 -0400

    Updated some colors and changed the navigation pane style

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
-function test() {
-  alert("TEST!");
-}
+const init = () => {
+  document.querySelector("#aboutLink").addEventListener("click", () => {
+    alert("You clicked about");
+  });
+  document.querySelector("#educationLink").addEventListener("click", () => {
+    alert("You clicked education");
+  })
+  document.querySelector("#experienceLink").addEventListener("click", () => {
+    alert("You clicked experience");
+  })
+  document.querySelector("#projectsLink").addEventListener("click", () => {
+    alert("You clicked project");
+  })
+  document.querySelector("#contactLink").addEventListener("click", () => {
+    alert("You clicked contact");
+  })
+  document.querySelector("#configLink").addEventListener("click", () => {
+    alert("You clicked config");
+  })
+};
